ca609e010a08ed9b5ff5f171ea1814e67ea16bd3
Iliyan Dimitrov <iliqn1994@gmail.com>
2020-02-03 15:08:09 +0000

    Initial program starting point

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 console.log("welcome to Game One");
+
+const readline = require("readline").createInterface({
+input: process.stdin,
+output: process.stdout
+});
+
+function handleUserResponse (response){
+
+    
+
+    console.log(response);
+    readline.close();
+}
+
+readline.question("Please choose either rock, paper or scissors! \n", handleUserResponse);
